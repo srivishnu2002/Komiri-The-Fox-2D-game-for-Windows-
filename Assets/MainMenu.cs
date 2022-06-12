a83a7d6bb96b7cb53e72b3f6b0c45daed4a9da64
SriVishnu Thatha <srivishnu.thatha123@gmail.com>
2022-06-12 10:56:25 -0700

    first commit

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+  public void PlayGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    public void HowToPlay()
+    {
+        SceneManager.LoadScene("howtoplay");
+    }
+    public void WhoMadeThisGame()
+    {
+        SceneManager.LoadScene("whomadethisgame");
+    }
+    public void BacktoFirst()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
